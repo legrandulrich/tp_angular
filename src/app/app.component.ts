8c0f300a1c0c952268b6526753b3d2ac1e6f8cce
legrandulrich <legrandulrich@gmail.com>
2024-01-06 19:22:17 -0500

    tp3 ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   username = '';
+  showSecret = false;
+  log =[];
 
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    //this.log.push(this.log.length+1);
+    this.log.push(new Date());
+  }
 }
